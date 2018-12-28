cda7f36d7aa591224dd2fcb426fcfa2478af253f
Croce Baptiste <crocebapti@eisti.eu>
2018-12-28 12:45:53 +0100

    amelioration graphic, quasi fini

--- testgraph.ml
+++ testgraph.ml
@@ -7,6 +7,7 @@ let k = 4 ;; (*nombre pions*)
 let c = 6 ;;(*nombre couleurs*)
 let l = [black;white;red;green;blue;yellow;cyan;magenta];;
 
+
 let rec liste_couleur l c  = 
 	match (l,c) with
 	|(h :: t, x) when x > 0 -> h :: liste_couleur t (x-1)
@@ -15,9 +16,12 @@ let rec liste_couleur l c  =
 
 let listecouleur = liste_couleur l c;;
 
-(*let rec ch_couleur couleur nb =
-	match (couleur,nb) with
-	|(co,x) -> *)
+let rec ch_couleur l couleur =
+	match l with
+	|(h :: []) -> if h = couleur then List.hd listecouleur else 0
+	|(h :: t) -> if h = couleur then List.hd t else ch_couleur t couleur 
+	|_ -> 0;;
+	
 
 let rec grille i j n = 
 	match (i,j,n) with 
@@ -48,24 +52,41 @@ rond red 564 53;;
 rond red 630 53;;
 rond red 696 53;;
 
+
+draw_rect 345 33 80 40;;
 moveto 350 53;;
 draw_string "cliquez ici" ;;
 moveto 350 43;;
-draw_string "pour choisir ->" ;;
-
-let bu = wait_next_event [Button_down];;
-
-(*let cliccouleur a b couleur =
-	if a >= 478 && a <= 518 && b >= 33 && b <= 73 then *)
-		
+draw_string "pour valider" ;;
 
 
+let cliccouleur a b couleur =
+	if a >= 478 && a <= 518 && b >= 33 && b <= 73 then 
+		rond (ch_couleur listecouleur couleur) 498 53 
+	else 
+	if a >= 544 && a <= 584 && b >= 33 && b <= 73 then 
+		rond (ch_couleur listecouleur couleur) 564 53 	
+	else
+	if a >= 610 && a <= 650 && b >= 33 && b <= 73 then 
+		rond (ch_couleur listecouleur couleur) 630 53 
+	else
+	if a >= 676 && a <= 716 && b >= 33 && b <= 73 then 
+		rond (ch_couleur listecouleur couleur) 696 53 
+	else draw_rect 10 10 1 1 ;;
+
+let valider a b add = 
+	if a >= 345 && a <= 425 && b >= 33 && b <= 73 then	
+		(rond (point_color 498 53) 498 (53 + add * 66);
+		rond (point_color 564 53) 564 (53 + add * 66);
+		rond (point_color 630 53) 630 (53 + add * 66);
+		rond (point_color 696 53) 696 (53 + add * 66))
+	else draw_rect 10 10 1 1 ;;
 
 
+let rec boucle ad = 
+	let bu = wait_next_event [Button_down] in
+	if bu.mouse_x >= 345 && bu.mouse_x <= 425 && bu.mouse_y >= 33 && bu.mouse_y <= 73 then
+		(valider bu.mouse_x bu.mouse_y ad ; boucle (ad+1)) else
+		(cliccouleur bu.mouse_x bu.mouse_y (point_color bu.mouse_x bu.mouse_y); boucle ad);;
 
-
-
-
-
-
-	
+boucle 0 ;;
