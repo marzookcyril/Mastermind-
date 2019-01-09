4b129be67fe870dc798d5ea51e222594e681a4fa
Baptiste Croce <crocebapti@eisti.eu>
2019-01-09 15:00:52 +0100

    graphique avec differents ecrans

--- interface.ml
+++ interface.ml
@@ -1,13 +1,11 @@
 open Graphics;;
 
-module Interface :
+(*module Interface :
 	sig 
 	
-		val depart : unit -> unit  
+		val menu : unit -> unit  
 
-		val boucle : int -> Graphics.color list -> unit
-
-	end = struct 
+	end = struct *)
 
 
 let depart () = 
@@ -20,7 +18,11 @@ let c = 6 ;;(**Nombre couleurs*)
 let t = 10 ;; (**Nombre tentatives*)
 let l = [black;white;red;green;blue;yellow];; (**Couleurs possibles*)
 
-depart ();;
+
+let accueil () = 
+	draw_rect 700 400 100 50;
+	moveto 720 420;
+	draw_string "Joueur vs IA" ;;
 
 
 let convert n = 
@@ -32,6 +34,7 @@ let convert n =
 	|a when a = blue -> "bleu"
 	|a when a = yellow  -> "jaune"
 	|_ -> "";;
+	
 
 let invconvert n = 
 	match n with 
@@ -81,12 +84,12 @@ let rec grille i j n =
 	|(x,y,z) when x < (t+1) && y = z+1 -> grille (x+1) 1 z;
 	|_                          -> draw_rect 10 10 1 1 ;;
 
-grille 1 1 k;;
 
 (**Delimitation grille/grille de reponse*)
-draw_rect (400 - 15*k) 86 (15*k + 60) (66*t);;
-fill_rect 460 86 5 (66*t);;
-draw_rect (400 - 15*k) 20 (15*k + 66*(k+1)) 66;;
+let grilleee () = 
+	draw_rect (400 - 15*k) 86 (15*k + 60) (66*t);
+	fill_rect 460 86 5 (66*t);
+	draw_rect (400 - 15*k) 20 (15*k + 66*(k+1)) 66;;
 
 (**Dessine la grille de reponse
 	*@param variable pour le dessin correct de la grille de reponse
@@ -96,7 +99,6 @@ let rec grillereponse acc =
 	|(x) when x < (t+1) -> draw_rect (400 - (15*k)) (20+(x*66)) (32*k) 0; grillereponse (x+1)
 	|_ -> draw_rect 10 10 1 1 ;;
 
-grillereponse 1;;
 
 (**Dessin d'un rond de la couleur voulue à l'endroit souhaité
 	*@param 	couleur voulue
@@ -113,14 +115,17 @@ let rond couleur x y =
 	*@rond 		cercles permettant de choisir les couleurs
 	*@rect 		rectangle pour valider son choix
 *)	
-let rondreponse = 
+let rondreponse () = 
 	for i = 0 to (k-1) do rond black (498 + i*66) 53 done;;
-	
-draw_rect 345 33 80 40;;
-moveto 350 53;;
-draw_string "cliquez ici" ;;
-moveto 350 43;;
-draw_string "pour valider" ;;
+
+
+let boutonvalider () =
+	draw_rect 345 33 80 40;
+	moveto 350 53;
+	draw_string "cliquez ici" ;
+	moveto 350 43;
+	draw_string "pour valider" ;;
+
 
 (**Change de couleur le rond sur lequel on clique 
 	*@param 	première coordonnée 
@@ -163,15 +168,98 @@ let rec recucode temp =
 	else
 		[];;
 		
-let o = recucode 0;;	
+let rec recucodesecret temp = 
+	if temp < k then
+		point_color (698 + temp*66) 363 :: recucodesecret (temp + 1)
+	else
+		[];;
+		
+let toutecreation () = 
+	grilleee ();
+	grille 1 1 k;
+	grillereponse 1;
+	rondreponse ();
+	boutonvalider ();;
 
+
+let chcouleurcode a b couleur = 
+	for i = 0 to k do 
+	if a >= (678 + i*66) && a <= (718 + i*66) && b >= 343 && b <= 383 then 
+		rond (ch_couleur listecouleur couleur) (698 +i*66) 363 
+	else draw_rect 10 10 1 1 done;;
+
+let rec couleurpourlecode a tableau = 
+	let bu = wait_next_event [Button_down] in
+	if bu.mouse_x >= 545 && bu.mouse_x <= 625 && bu.mouse_y >= 343 && bu.mouse_y <= 383 then
+		let tableau = (recucodesecret 0) in print_string (convert (List.hd tableau))
+	else
+		(chcouleurcode bu.mouse_x bu.mouse_y (point_color bu.mouse_x bu.mouse_y); couleurpourlecode a tableau);;
+
+let choixducodesecret () =
+	draw_rect (600 - 15*k) 334 (15*k + 66*(k+1)) 66;
+	draw_rect 700 400 100 50;
+	moveto 720 420;
+	draw_string "Choix code" ;
+	for i = 0 to (k-1) do rond black (698 + i*66) 363 done;
+	draw_rect 545 344 80 40;
+	moveto 550 364;
+	draw_string "cliquez ici" ;
+	moveto 550 354;
+	draw_string "pour valider" ;
+	couleurpourlecode 0 [];;
+
+	
+	
 (**Boucle principale permettant de jouer tout le reste*)
-let rec boucle ad tableau = 
+let rec boucle level ad tableau = 
 	if ad > t then (moveto 550 (75 * t) ; draw_string "C'est perdu.") else 
 	let bu = wait_next_event [Button_down] in
 	if bu.mouse_x >= 345 && bu.mouse_x <= 425 && bu.mouse_y >= 33 && bu.mouse_y <= 73 then
-		(valider bu.mouse_x bu.mouse_y ad ; pionplace (Random.int 5) (Random.int 5) ad; let tableau = (recucode 0) in if tableau != [] then rond (List.hd tableau) 10 10 ; let tableau = (recucode 0) in boucle (ad+1) tableau) else
-		(cliccouleur bu.mouse_x bu.mouse_y (point_color bu.mouse_x bu.mouse_y); boucle ad tableau);;
-boucle 0 [];;
+		(valider bu.mouse_x bu.mouse_y ad ; pionplace (Random.int 5) (Random.int 5) ad; let tableau = (recucode 0) in boucle level (ad+1) tableau) else
+		(cliccouleur bu.mouse_x bu.mouse_y (point_color bu.mouse_x bu.mouse_y); boucle level ad tableau);;
+		
+		
+let choixniveau () = 
+	draw_rect 350 400 100 50;
+	moveto 370 420;
+	draw_string "Level 1" ;
+	
+	draw_rect 600 400 100 50;
+	moveto 620 420;
+	draw_string "Level 2" ;
+	
+	draw_rect 850 400 100 50;
+	moveto 870 420;
+	draw_string "Level 3" ;
+	
+	set_color red;
+	fill_rect 1100 400 100 50;
+	moveto 1120 420;
+	set_color black;
+	draw_string "Level 4" ;
+	
+	let bu = wait_next_event [Button_down] in
+		if bu.mouse_x >= 350 && bu.mouse_x <= 450 && bu.mouse_y >= 400 && bu.mouse_y <= 450 then
+			(clear_graph () ; toutecreation () ; boucle 1 0 [])
+		else 
+			if bu.mouse_x >= 600 && bu.mouse_x <= 700 && bu.mouse_y >= 400 && bu.mouse_y <= 450 then
+				(clear_graph () ;toutecreation () ; boucle 2 0 [])
+			else
+				if bu.mouse_x >= 850 && bu.mouse_x <= 950 && bu.mouse_y >= 400 && bu.mouse_y <= 450 then
+					(clear_graph () ; toutecreation () ; boucle 3 0 [])
+				else 
+					if bu.mouse_x >= 1100 && bu.mouse_x <= 1200 && bu.mouse_y >= 400 && bu.mouse_y <= 450 then
+						(clear_graph () ; toutecreation () ; boucle 4 0 []);;
 
-end;;
+let rec menu () =
+	depart();
+	accueil();
+	let bu = wait_next_event [Button_down] in
+		if bu.mouse_x >= 700 && bu.mouse_x <= 800 && bu.mouse_y >= 400 && bu.mouse_y <= 450 then
+			(clear_graph() ; choixniveau ())
+		else menu ();;
+		
+depart ();;
+choixducodesecret();;
+(*menu ();; *)
+(*end;; *)
